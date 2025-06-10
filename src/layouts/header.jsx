123a0b11b28dc6efe2c315382086fe66214b11a7
okechukwu samuel okolie <okechukwu.okolie.2023@gmail.com>
2025-06-10 04:44:45 +0100

    second commit into dashboard frontend

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,3 +1,17 @@
-export const Header = () => {
-  return <header>Header</header>;
+import { ChevronsLeft } from "lucide-react";
+import PropTypes from "prop-types";
+
+export const Header = ({collapsed,setCollasped}) => {
+  return <header className="relative z-10 h-[60px] items-center justify-between bg-white px-4 shadow-md transition-colors dark:bg-slate-900">
+    <div className="flex items-center gap-x-3">
+      <button className="btn-ghost size-10"  onClick={()=>setCollasped(!collapsed)}>
+        <ChevronsLeft className={collapsed && "rotate-180"} />
+      </button>
+    </div>
+  </header>;
 };
+
+Header.propTypes={
+  collapsed:PropTypes.bool,
+  setCollasped:PropTypes.func
+}
--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -12,14 +12,15 @@ const Layout = () => {
   const sidebarRef = useRef(null);
 
   return (
-    <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
+    <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-950">
       <div className={cn("pointer-event-none fixed inset-0 -z-10  bg-black opacity-0 transition-opacity", !collapsed && "max-md:pointer-events-auto max-md:opacity-30 max-md:z-50 , ")}></div>
       <Sidebar
         ref={sidebarRef}
         collapsed={collapsed}
       />
       <div className={cn("transition-[margin] duration-300", collapsed ?'md:ml-[78px]':'md:ml-[240px]')}>
-        <Header />
+        <Header collapsed={collapsed}
+        setCollapsed={setCollapsed} />
         <div className="mx-6 h-[calc(100vh-60px)] overflow-y-auto overflow-x-hidden">
           <Outlet />
         </div>
